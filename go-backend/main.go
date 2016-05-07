be4b7b32aab5616c514ef127984369bf19238d8e
Yury Paraschenko <y_paraschenko@wargaming.net>
2016-05-07 19:11:46 +0300

    go-backend: added args

--- go-backend/main.go
+++ go-backend/main.go
@@ -1,21 +1,29 @@
 package main
 
-//import "fmt"
 import "github.com/vvampirius/bookmarks/go-backend/bookmarks"
-
-//import "github.com/vvampirius/bookmarks/go-backend/bookmarks/bookmark"
-
-//import "os"
-//import "path"
+import "os"
 import "net/http"
 import "log"
+import "flag"
 
 func main() {
-	bs := bookmarks.Bookmarks{"/tmp"}
+	listen := flag.String("l", ":9090", "listen [<IP>]:<port>")
+	flag.Parse()
+	dir := flag.Arg(0)
+	if dir == "" {
+		log.Fatal("You must specify directory with bookmarks!")
+	}
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+	if dirStat, _ := os.Stat(dir); dirStat.IsDir() != true {
+		log.Fatal(dir + " is not directory!")
+	}
+	bs := bookmarks.Bookmarks{dir}
 	http.HandleFunc("/get/", bs.HttpGet)
 	http.HandleFunc("/add/", bs.HttpAdd)
 	http.HandleFunc("/list/", bs.HttpList)
-	err := http.ListenAndServe(":9090", nil) // set listen port
+	err := http.ListenAndServe(*listen, nil) // set listen port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
